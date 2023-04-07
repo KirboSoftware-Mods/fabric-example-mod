c72f77ca75ffa0953b7916b15036033c1edcbb5a
JoostMSoftware <joostmsoftware@gmail.com>
2023-04-07 10:28:24 +0200

    Added accesswidener, datagen, some nice plugins, moji as default and correct namespace.

---
+++ src/main/java/com/joostmsoftware/example/data/ExampleModDataEntrypoint.java
@@ -0,0 +1,11 @@
+package com.joostmsoftware.example.data;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ExampleModDataEntrypoint implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+    }
+}
